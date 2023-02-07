0f7c319fe024218aa42ef137590e24953ed50b02
Oakka1812 <soeoakka@gamil.com>
2023-02-07 20:12:16 +0630

    filtering product category

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,10 @@
 
 const app = document.querySelector("#app");
 const productCards = document.querySelector("#productCards");
+const productCategories = document.querySelector("#productCategories");
 const productCardDetail = new bootstrap.Modal("#productCardDetailModal");
+
+const categories = [...new Set(products.map((product) => product.category))];
 
 const star = (num) => {
   let star = "";
@@ -16,6 +19,18 @@ const star = (num) => {
   return star;
 };
 
+const slugToText = (slug) => {
+  return slug.replaceAll("-", " ");
+};
+
+const createCategoryBtn = (name) => {
+  const btn = document.createElement("button");
+  btn.className = "btn btn-sm btn-outline-dark text-capitalize me-2 cat";
+  btn.innerText = slugToText(name);
+  btn.setAttribute("cat", name);
+  return btn;
+};
+
 const createProductCard = (product) => {
   const div = document.createElement("div");
   div.className = "col-12 col-md-6 col-lg-4 product-card";
@@ -25,9 +40,8 @@ const createProductCard = (product) => {
     <img src="${product.thumbnail}" alt="" class="product-img">
     <h3 class="fw-bold mt-2 mb-2 text-truncate">${product.title}</h3>
     <div class="d-flex justify-content-between align-items-center mb-2">
-        <div class="badge bg-secondary text-capitalize">${product.category.replaceAll(
-          "-",
-          ""
+        <div class="badge bg-secondary text-capitalize">${slugToText(
+          product.category
         )}</div>
         <div>
             ${star(product.rating)}
@@ -47,20 +61,86 @@ const createProductCard = (product) => {
   return div;
 };
 
-products.forEach((product) => {
-  // console.log(products);
-  productCards.append(createProductCard(product));
-});
+const renderProductCard = (products) => {
+  productCards.innerHTML = null;
+  products.forEach((product) => {
+    productCards.append(createProductCard(product));
+  });
+};
+
+const renderProductDetailModal = () => {
+  const currentCard = event.target.closest(".product-card");
+  const currentProductId = currentCard.getAttribute("product-id");
+  const currentProduct = products.find(
+    (product) => product.id == currentProductId
+  );
+  productCardDetail._element.querySelector(".modal-title").innerText =
+    currentProduct.title;
+
+  productCardDetail._element.querySelector(
+    ".modal-body"
+  ).innerHTML = `<div id="productDetailCarousel" class="carousel slide">
+      <div class="carousel-indicators">
+       ${productDetailCarouselItem(currentProduct.images).indicators}
+      </div>
+      <div class="carousel-inner">
+      
+        ${productDetailCarouselItem(currentProduct.images).slides}
+      </div>
+      <div class="d-flex justify-content-between align-items-center mt-3">
+          <div class="badge bg-secondary text-capitalize">${slugToText(
+            currentProduct.category
+          )}</div>
+          <div>
+              ${star(currentProduct.rating)}
+          </div>
+      </div>
+      <p class="mt-3">${currentProduct.description}</p>
+      <p>$ ${currentProduct.price}</p>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#productDetailCarousel"
+        data-bs-slide="prev"
+      >
+        <span
+          class="carousel-control-prev-icon"
+          aria-hidden="true"
+        ></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#productDetailCarousel"
+        data-bs-slide="next"
+      >
+        <span
+          class="carousel-control-next-icon"
+          aria-hidden="true"
+        ></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div> `;
+  productCardDetail.show();
+};
+
+categories.forEach((category) =>
+  productCategories.append(createCategoryBtn(category))
+);
+
+renderProductCard(products);
 
 const productDetailCarouselItem = (arr) => {
-    let slides = "";
-    let indicators = "";
-    arr.forEach((e, index) => {
-        slides += `<div class="carousel-item ${index === 0 && "active"}">
+  let slides = "";
+  let indicators = "";
+  arr.forEach((e, index) => {
+    slides += `<div class="carousel-item ${index === 0 && "active"}">
         <img src="${e}" class="d-block w-100 product-detail-img" alt="..." />
       </div>`;
 
-      indicators += `<button
+    indicators += `<button
       type="button"
       data-bs-target="#productDetailCarousel"
       data-bs-slide-to="${index}"
@@ -68,68 +148,20 @@ const productDetailCarouselItem = (arr) => {
       aria-current="true"
       aria-label="Slide ${index}"
     ></button>`;
-    })
-    return {slides, indicators};
-}
+  });
+  return { slides, indicators };
+};
 
 app.addEventListener("click", (event) => {
   // console.log(event.target);
   if (event.target.closest(".product-card")) {
-    const currentCard = event.target.closest(".product-card");
-    const currentProductId = currentCard.getAttribute("product-id");
-    const currentProduct = products.find(
-      (product) => product.id == currentProductId
-    );
-    productCardDetail._element.querySelector(".modal-title").innerText =
-      currentProduct.title;
+    renderProductDetailModal();
+  }
 
-    productCardDetail._element.querySelector(
-      ".modal-body"
-    ).innerHTML = `<div id="productDetailCarousel" class="carousel slide">
-        <div class="carousel-indicators">
-         ${productDetailCarouselItem(currentProduct.images).indicators}
-        </div>
-        <div class="carousel-inner">
-        
-          ${productDetailCarouselItem(currentProduct.images).slides}
-        </div>
-        <div class="d-flex justify-content-between align-items-center mt-3">
-            <div class="badge bg-secondary text-capitalize">${currentProduct.category.replaceAll(
-              "-",
-              ""
-            )}</div>
-            <div>
-                ${star(currentProduct.rating)}
-            </div>
-        </div>
-        <p class="mt-3">${currentProduct.description}</p>
-        <p>$ ${currentProduct.price}</p>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#productDetailCarousel"
-          data-bs-slide="prev"
-        >
-          <span
-            class="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#productDetailCarousel"
-          data-bs-slide="next"
-        >
-          <span
-            class="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div> `;
-    productCardDetail.show();
+  if(event.target.classList.contains("cat")){
+    // console.log(event.target);
+    // console.log(products);
+    // console.log(event.target.getAttribute("cat"))
+    renderProductCard(products.filter((product) => product.category === event.target.getAttribute("cat")))
   }
 });
